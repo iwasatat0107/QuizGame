9f340783b1467fa531889d74ef17a6c5c408ed00
iwasatat0107 <it.was07@gmail.com>
2021-02-02 06:41:35 +0900

    問題文の1

---
+++ app.js
@@ -0,0 +1,12 @@
+const question = 'ゲーム市場で、最も売れたゲーム機は次のうちどれ？';
+const answers = [
+  'スーパーファミコン',
+  'プレイステーション2',
+  'ニンテンドースイッチ',
+  'ニンテンドーDS'
+];
+ const correct = 'ニンテンドーDS';
+
+ console.log();
+
+document.getElementById('js-question').textContent = question;
