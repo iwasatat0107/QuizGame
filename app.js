e2df85a3b27e132ef8b46b0471476e896703f9ac
iwasatat0107 <it.was07@gmail.com>
2021-02-03 06:53:16 +0900

    正誤判定の作成

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ const answers = [
 
 document.getElementById('js-question').textContent = question;
 
+//定数の文字列をHTMLに反映させる
 document.getElementsByTagName('button')[0].textContent = answers[0];
 document.getElementsByTagName('button')[1].textContent = answers[1];
 document.getElementsByTagName('button')[2].textContent = answers[2];
 document.getElementsByTagName('button')[3].textContent = answers[3];
+
+//ボタンをクリックしたら正誤判定
+document.getElementsByTagName('button')[0].addEventListener('click',() => {
+ if(correct === document.getElementsByTagName('button')[0].textContent){
+    window.alert('正解！');
+ } else {
+    window.alert('不正解！');
+ }
+});
